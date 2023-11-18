5a56c9f016421d23cf84e14f24fbdc4ae4027ac5
NextDev65 <12612637+NextDev65@users.noreply.github.com>
2023-11-18 13:10:09 -0600

    temp fix (GetHub for dl and winget for uninstall)

--- appscmd/PVPN.ps1
+++ appscmd/PVPN.ps1
@@ -1,3 +1,16 @@
+# this script installs, runs, then uninstalls protonvpn
+# choco protonvpn is broken: 3.2.4 is outdated but also not available from source (github)
+
+<#
 choco upgrade protonvpn -y
 Start-Process 'C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Proton\Proton VPN.lnk' -WindowStyle Hidden -Wait
 choco uninstall protonvpn
+#>
+
+$setup = GetHub -Owner 'ProtonVPN' -Repo 'win-app' -RegexPattern 'ProtonVPN_v.*\.exe'
+$tempDir = [System.IO.Path]::GetTempPath()
+Move-Item $setup $tempDir
+$setupPath = Join-Path $tempDir $setup
+Start-Process $setupPath -ArgumentList "/silent" -Wait
+Remove-Item $setupPath
+winget uninstall 'Proton VPN_is1'
