c45ac6dc47e7796de50ce89f98bbadc0fa8c9f3a
NextDev65 <12612637+NextDev65@users.noreply.github.com>
2023-12-24 22:31:05 -0600

    formatting + remove pause

--- appscmd/android/adb56.bat
+++ appscmd/android/adb56.bat
@@ -2,20 +2,19 @@
 
 set andip=noip
 ::A
-FOR /F "tokens=9 USEBACKQ" %%F IN (`adb shell ip route`) DO (SET andip=%%F)
-::FOR /F "tokens=9 USEBACKQ" %%F IN (`adb shell ip route`) DO (ECHO %%F & SET andip=%%F)
+FOR /F "tokens=9 USEBACKQ" %%F IN (`adb shell ip route`) DO (SET andip=%%F):: && ECHO %%F)
 ::B
-::FOR /F "tokens=2 USEBACKQ" %%F IN (`adb shell ip addr show wlan0 ^| findstr /e wlan0`) DO (ECHO %%F & SET andip=%%F)
+::FOR /F "tokens=2 USEBACKQ" %%F IN (`adb shell ip addr show wlan0 ^| findstr /e wlan0`) DO (SET andip=%%F):: && ECHO %%F)
 ::set andip=%andip:~0, -3%
 ::echo %andip%
 
 ::if not "x%andip:192=%"=="x%andip%" echo %andip% > "%~dp0.cfg\andip.txt"
-if not "%andip%"=="noip" echo %andip% > "%~dp0.cfg\andip.txt"
-set /p andip=< "%~dp0.cfg\andip.txt"
-set andip=%andip:~0, -1%
+if not "%andip%"=="noip" (echo %andip%>"%~dp0.cfg\andip.txt")
+::set /p andip=<"%~dp0.cfg\andip.txt"
 
-::adb tcpip 5656
-::call adb connect %andip%:5656
 adb tcpip 41315
 call adb connect %andip%:41315
-::pause
+::adb tcpip 5656
+::call adb connect %andip%:5656
+
+EXIT /b
