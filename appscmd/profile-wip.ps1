94256b915504ffbdb7901418d16a595c1e437f0b
NextDev65 <12612637+NextDev65@users.noreply.github.com>
2024-01-10 13:35:52 -0600

    profile-wip.ps1: add touch and choco-up

--- appscmd/profile-wip.ps1
+++ appscmd/profile-wip.ps1
@@ -1,3 +1,18 @@
+# https://superuser.com/questions/502374/equivalent-of-linux-touch-to-create-an-empty-file-with-powershell
+Function touch {
+    foreach ($file in $args) {
+        if ($file -eq $null) {
+            throw "No filename supplied"
+        }
+
+        if (Test-Path $file) {
+            (Get-ChildItem $file).LastWriteTime = Get-Date
+        } else {
+            New-Item $file
+        }
+    }
+}
+
 # https://stackoverflow.com/questions/24914589/how-to-create-permanent-powershell-aliases
 Function choco-u { choco upgrade chocolatey }
 Function choco-un {
@@ -6,4 +21,15 @@ Function choco-un {
     )
     
     choco uninstall $packageName --remove-dependencies
+}
+Function choco-up {
+    Write-Host "Listing outdated packages..."
+    choco outdated
+
+    $choice = Read-Host "Do you want to upgrade all outdated packages? (y/n)"
+    if ($choice -eq 'y') {
+        choco upgrade all -y
+    } else {
+        Write-Host "Upgrade canceled."
+    }
 }
