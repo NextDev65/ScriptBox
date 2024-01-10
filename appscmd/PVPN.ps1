0908185c05fdee500426597b7970507f693d58e9
NextDev65 <12612637+NextDev65@users.noreply.github.com>
2024-01-10 13:11:10 -0600

    PVPN.ps1: choco protonvpn is no longer broken, reverted to choco and commented out GetHub

--- appscmd/PVPN.ps1
+++ appscmd/PVPN.ps1
@@ -1,16 +1,18 @@
-# this script installs, runs, then uninstalls protonvpn
-# choco protonvpn is broken: 3.2.4 is outdated but also not available from source (github)
+# this script installs, runs, and then uninstalls protonvpn
 
-<#
 choco upgrade protonvpn -y
 Start-Process 'C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Proton\Proton VPN.lnk' -WindowStyle Hidden -Wait
 choco uninstall protonvpn
+
+<#
+$setup = GetHub -Owner 'ProtonVPN' -Repo 'win-app' -RegexPattern 'ProtonVPN_v.*\.exe'
+
+Write-Host "Running $setup"
+#PS5# (Invoke-Expression $setup) -and (Remove-Item $setup)
+#PS7#
+Start-Process $setup -ArgumentList "/silent" -Wait && Remove-Item $setup
+
+winget uninstall 'Proton VPN_is1'
 #>
 
-$setup = GetHub -Owner 'ProtonVPN' -Repo 'win-app' -RegexPattern 'ProtonVPN_v.*\.exe'
-$tempDir = [System.IO.Path]::GetTempPath()
-Move-Item $setup $tempDir
-$setupPath = Join-Path $tempDir $setup
-Start-Process $setupPath -ArgumentList "/silent" -Wait
-Remove-Item $setupPath
-winget uninstall 'Proton VPN_is1'
+Exit
