55565114c741b2680284f505954aff8ef4d8d29b
NextDev65 <12612637+NextDev65@users.noreply.github.com>
2024-02-01 16:35:49 -0600

    winget-up.ps1: add Portmaster, (PowerShell) don't opt into ms update, (PowerShell) switch to interactive to disable shortcut creation

--- appscmd/winget-up.ps1
+++ appscmd/winget-up.ps1
@@ -1,13 +1,22 @@
+#TODO
+# make upgrade function
+#  exact id match `--exact`
+#https://learn.microsoft.com/en-us/windows/package-manager/winget/install
+
 $Params = @(
+    "USE_MU=0"
+    "ENABLE_MU=0"
     "ADD_EXPLORER_CONTEXT_MENU_OPENPOWERSHELL=0"
     "ADD_FILE_CONTEXT_MENU_RUNPOWERSHELL=0"
     "ADD_PATH=1"
     "DISABLE_TELEMETRY=1"
 )
-winget upgrade --silent --id Microsoft.PowerShell --source winget --custom ($Params -join " ")
+winget upgrade --interactive --id Microsoft.PowerShell --source winget --custom ($Params -join " ")
+#winget upgrade --silent --id Microsoft.PowerShell --source winget --custom ($Params -join " ")
 echo DOTNET_CLI_TELEMETRY_OPTOUT $env:DOTNET_CLI_TELEMETRY_OPTOUT
 echo POWERSHELL_TELEMETRY_OPTOUT $env:POWERSHELL_TELEMETRY_OPTOUT
 
+winget upgrade --silent --id Safing.Portmaster --source winget
 winget upgrade --silent --id Microsoft.PowerToys --source winget
 winget upgrade --silent --id Microsoft.WindowsTerminal --source winget
 
