648a4447cddf75f7488e840894a75cd031fd6bdc
NextDev65 <12612637+NextDev65@users.noreply.github.com>
2024-02-01 16:15:02 -0600

    pip-pin.bat: save requirements to "requirements-pinned.txt"

--- appscmd/pip-pin.bat
+++ appscmd/pip-pin.bat
@@ -1,3 +1,7 @@
 @echo off
 :: https://stackoverflow.com/a/40026169
-pip freeze -r requirements.txt | wsl sed '/freeze/,$ d'
+
+pip freeze -r requirements.txt | wsl sed '/freeze/,$ d' > requirements-pinned.txt
+type requirements-pinned.txt
+
+EXIT /b
