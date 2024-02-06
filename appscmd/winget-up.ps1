d3ef3134e4c5f95417de0e9825505bb45d7724de
NextDev65 <12612637+NextDev65@users.noreply.github.com>
2024-02-06 12:19:13 -0600

    winget-up.ps1: Add SophiApp, add comments, remove printing telemetry env vars

--- appscmd/winget-up.ps1
+++ appscmd/winget-up.ps1
@@ -3,6 +3,7 @@
 #  exact id match `--exact`
 #https://learn.microsoft.com/en-us/windows/package-manager/winget/install
 
+# PowerShell 7
 $Params = @(
     "USE_MU=0"
     "ENABLE_MU=0"
@@ -13,14 +14,27 @@ $Params = @(
 )
 winget upgrade --interactive --id Microsoft.PowerShell --source winget --custom ($Params -join " ")
 #winget upgrade --silent --id Microsoft.PowerShell --source winget --custom ($Params -join " ")
-echo DOTNET_CLI_TELEMETRY_OPTOUT $env:DOTNET_CLI_TELEMETRY_OPTOUT
-echo POWERSHELL_TELEMETRY_OPTOUT $env:POWERSHELL_TELEMETRY_OPTOUT
+#echo DOTNET_CLI_TELEMETRY_OPTOUT $env:DOTNET_CLI_TELEMETRY_OPTOUT
+#echo POWERSHELL_TELEMETRY_OPTOUT $env:POWERSHELL_TELEMETRY_OPTOUT
 
+# Firewall
 winget upgrade --silent --id Safing.Portmaster --source winget
+
+# MS PowerToys
 winget upgrade --silent --id Microsoft.PowerToys --source winget
+
+# Windows Terminal
 winget upgrade --silent --id Microsoft.WindowsTerminal --source winget
 
+# Android Mirroring
 winget upgrade --silent --id Genymobile.scrcpy --source winget
+
+# Notes
 winget upgrade --silent --id LinwoodCloud.Butterfly --source winget
+
+# Focus Timer
 winget upgrade --silent --id YetAnotherPomodoroApp.YAPA2 --source winget
 winget upgrade --silent --id zxch3n.PomodoroLogger --source winget
+
+# Windows Tweaks
+winget upgrade --silent --id TeamSophia.SophiApp --source winget
