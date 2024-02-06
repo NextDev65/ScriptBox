16eb873f92d5c7fe516815b9f12f7d602a33c6d1
NextDev65 <12612637+NextDev65@users.noreply.github.com>
2024-02-06 12:15:39 -0600

    pip-default.bat: add comments and new packages (commented out)

--- appscmd/pip-default.bat
+++ appscmd/pip-default.bat
@@ -1,7 +1,15 @@
 @echo off
 
+:: virtual env
 pip install pipenv
+:: whl binary support
 pip install wheel
+:: autoremove dependencies
 pip install python3-pip-autoremove
+
+:: pep8 checker
+::pip install pycodestyle
+:: pep8 autoformatting
+::pip install black
 
 EXIT /b
