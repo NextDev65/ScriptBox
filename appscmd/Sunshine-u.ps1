2d827d105254014111859ed02888ed1ea658e217
NextDev65 <12612637+NextDev65@users.noreply.github.com>
2024-06-07 16:14:44 -0500

    Sunshine-u.ps1: download installer in Temp folder

--- appscmd/Sunshine-u.ps1
+++ appscmd/Sunshine-u.ps1
@@ -1,5 +1,12 @@
+$tempDir = [System.IO.Path]::GetTempPath()
+Set-Location -Path $tempDir
+
 $assetName = GetHub -Owner 'LizardByte' -Repo 'Sunshine' -FileName 'sunshine-windows-installer.exe'
+
 Write-Host "Running $assetName"
 #PS5# (Invoke-Expression $assetName) -and (Remove-Item $assetName)
 #PS7#
 Start-Process $assetName -Wait && Remove-Item $assetName
+
+# return to current dir
+Set-Location -Path $CurrentDirectory
