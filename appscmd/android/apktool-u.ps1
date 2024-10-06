f549e8e685ea6a1c028804ec9f6cce3013f6a8f4
NextDev65 <12612637+NextDev65@users.noreply.github.com>
2024-10-06 03:44:32 -0500

    Add apktool-u.ps1: update apktool

---
+++ appscmd/android/apktool-u.ps1
@@ -0,0 +1,14 @@
+﻿# go to script's dir
+$CurrentDirectory = Get-Location
+Set-Location -Path $PSScriptRoot
+
+# jar
+Remove-Item -Path 'apktool_*.jar'
+$assetName = GetHub -Owner 'iBotPeaches' -Repo 'Apktool' -RegexPattern 'apktool_.*\.jar'
+# runner bat
+Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/iBotPeaches/Apktool/master/scripts/windows/apktool.bat' -OutFile 'apktool.bat'
+
+# return to current dir
+Set-Location -Path $CurrentDirectory
+
+Exit
